849bd5b0a73542e14afa7b252f2847befe9e8698
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2020-06-21 14:48:59 -0700

    Generation of C++ ser/deser functions works
    Still need to make deserialization more robust my making sure we haven't
    reached the end iterator of the vector.

--- messages/compiler/serialize.h
+++ messages/compiler/serialize.h
@@ -13,19 +13,35 @@
 
 #pragma once
 
+#include <algorithm>
+#include <cassert>
 #include <cstdint>
-#include <vector>
+#include <map>
+#include <optional>
+#include <string>
 #include <type_traits>
+#include <variant>
+#include <vector>
 
 namespace cmf {
 
-// All integers are encoded in little endian
+class DeserializeError : public std::runtime_error {
+ public:
+  DeserializeError(const std::string& error) : std::runtime_error(("DeserializeError: " + error).c_str()) {}
+  const char* what() const noexcept override { return std::runtime_error::what(); }
+};
+
+/******************************************************************************
+ * Integers
+ *
+ * All integers are encoded in little endian
+ ******************************************************************************/
 template <typename T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
 void Serialize(std::vector<uint8_t>& output, const T& t) {
   if constexpr (std::is_same_v<T, const bool&>) {
     output.push_back(t ? 1 : 0);
   } else {
-    for (auto i = 0; i < sizeof(T); i++) {
+    for (auto i = 0u; i < sizeof(T); i++) {
       output.push_back(255 & t >> (0 * 8));
     }
   }
@@ -35,10 +51,124 @@ template <typename T, typename std::enable_if<std::is_integral<T>::value>::type*
 void Deserialize(std::vector<uint8_t>::const_iterator& start, T& t) {
   if constexpr (std::is_same_v<T, const bool&>) {
     (*start == 0) ? t = true : t = false;
+    start += 1;
   } else {
-    for (auto i = 0; i < sizeof(T); i++) {
+    for (auto i = 0u; i < sizeof(T); i++) {
       t |= ((*start + i) << 8 * i);
     }
+    start += sizeof(T);
+  }
+}
+
+/******************************************************************************
+ * Strings
+ *
+ * Strings are preceded by a uint32_t length
+ ******************************************************************************/
+void Serialize(std::vector<uint8_t>& output, const std::string& s) {
+  assert(s.size() <= 0xFFFFFFFF);
+  uint32_t length = s.size() & 0xFFFFFFFF;
+  Serialize(output, length);
+  std::copy(s.begin(), s.end(), std::back_inserter(output));
+}
+
+void Deserialize(std::vector<uint8_t>::const_iterator& start, std::string& s) {
+  uint32_t length;
+  Deserialize(start, length);
+  std::copy_n(start, length, std::back_inserter(s));
+  start += length;
+}
+
+/******************************************************************************
+ * Lists are modeled as std::vectors
+ *
+ * Lists are preceded by a uint32_t length
+ ******************************************************************************/
+template <typename T>
+void Serialize(std::vector<uint8_t>& output, const std::vector<T>& v) {
+  assert(v.size() <= 0xFFFFFFFF);
+  uint32_t length = v.size() & 0xFFFFFFFF;
+  Serialize(output, length);
+  for (auto& it : v) {
+    Serialize(output, it);
+  }
+}
+
+template <typename T>
+void Deserialize(std::vector<uint8_t>::const_iterator& start, std::vector<T>& v) {
+  uint32_t length;
+  Deserialize(start, length);
+  for (auto i = 0u; i < length; i++) {
+    T t;
+    Deserialize(start, t);
+    v.push_back(t);
+  }
+}
+
+/******************************************************************************
+ * KVPairs are modeled as std::pairs.
+ ******************************************************************************/
+template <typename K, typename V>
+void Serialize(std::vector<uint8_t>& output, const std::pair<K, V>& kvpair) {
+  Serialize(output, kvpair.first);
+  Serialize(output, kvpair.second);
+}
+
+template <typename K, typename V>
+void Deserialize(std::vector<uint8_t>::const_iterator& start, std::pair<K, V>& kvpair) {
+  Deserialize(start, kvpair.first);
+  Deserialize(start, kvpair.second);
+}
+
+/******************************************************************************
+ * Maps
+ *
+ * Maps are preceded by a uint32_t size
+ ******************************************************************************/
+template <typename K, typename V>
+void Serialize(std::vector<uint8_t>& output, const std::map<K, V>& m) {
+  assert(m.size() <= 0xFFFFFFFF);
+  uint32_t size = m.size() & 0xFFFFFFFF;
+  Serialize(output, size);
+  for (auto& it : m) {
+    Serialize(output, it);
+  }
+}
+
+template <typename K, typename V>
+void Deserialize(std::vector<uint8_t>::const_iterator& start, std::map<K, V>& m) {
+  uint32_t size;
+  Deserialize(start, size);
+  for (auto i = 0u; i < size; i++) {
+    std::pair<K, V> kvpair;
+    Deserialize(start, kvpair);
+    m.insert(kvpair);
+  }
+}
+
+/******************************************************************************
+ * Optionals are modeled as std::optional
+ *
+ * Optionals are preceded by a bool indicating whether a value is present or not.
+ ******************************************************************************/
+template <typename T>
+void Serialize(std::vector<uint8_t>& output, const std::optional<T>& t) {
+  Serialize(output, t.has_value());
+  if (t.has_value()) {
+    Serialize(output, t.value());
+  }
+}
+
+template <typename T>
+void Deserialize(std::vector<uint8_t>::const_iterator& start, std::optional<T>& t) {
+  bool has_value;
+  Deserialize(start, has_value);
+  if (has_value) {
+    T value;
+    Deserialize(start, value);
+    t = value;
+  } else {
+    t = std::nullopt;
   }
 }
 
