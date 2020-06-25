2906b08a429392befa3f9f55cd105b2ee8c1a828
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2020-06-24 20:19:28 -0700

    Generate test message instances with variable sized sequences

--- messages/compiler/cpp/test_cppgen.py
+++ messages/compiler/cpp/test_cppgen.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import random
 import sys
 
@@ -8,13 +9,16 @@ import cppgen
 import cmfc
 from exceptions import CmfParseError
 
+MAX_SIZE = 5
+POSSIBLE_EMPTY_SIZES = [MAX_SIZE - i for i in range(0, math.ceil(MAX_SIZE / 2))]
+
 
 def randint():
     return random.randint(0, 100)
 
 
 def randstring():
-    return '"' + random.choice(["a", "b", "c", "aa" "bb" "cc" "abcdef"]) + '"'
+    return '"' + random.choice(["a", "b", "c", "aa", "bb", "cc", "abcdef"]) + '"'
 
 
 def byteExample():
@@ -42,13 +46,24 @@ class InstanceGen:
                 return random.choice(["true", "false"])
             # The type must be the name of a message
             return self.inlineStructInstanceStr(self.msgmap[type])
-        elif self.size == 0:
+        if self.size == 0:
             return "{}"
+        if self.size in POSSIBLE_EMPTY_SIZES:
+            # Return empty values occasionally to get random empties in nested types
+            # We do this for larger sizes so that we always get nested types containing 0, 1, or 2 entries.
+            if random.randint(0, 1) == 0:
+                return "{}"
 
         # TODO: Support multiple values in a type according to size
-        elif "list" in type:
-            return "{" + self.instancestr(type.list.type) + "}"
-        elif "kvpair" in type:
+        if "list" in type:
+            return (
+                "{"
+                + ",".join(
+                    [self.instancestr(type.list.type) for _ in range(0, self.size)]
+                )
+                + "}"
+            )
+        if "kvpair" in type:
             return (
                 "{"
                 + self.instancestr(type.kvpair.key)
@@ -56,18 +71,25 @@ class InstanceGen:
                 + self.instancestr(type.kvpair.value)
                 + "}"
             )
-        elif "map" in type:
-            # Maps require an initializer list of kv pairs, which themselves are represented as initializer lists. That's the reason for the double bracket.
+        if "map" in type:
+            # Maps require an initializer list of std::pairs, which themselves are represented as initializer lists.
             return (
-                "{{"
-                + self.instancestr(type.map.key)
-                + ", "
-                + self.instancestr(type.map.value)
-                + "}}"
+                "{"
+                + ",".join(
+                    [
+                        "{"
+                        + self.instancestr(type.map.key)
+                        + ", "
+                        + self.instancestr(type.map.value)
+                        + "}"
+                        for _ in range(0, self.size)
+                    ]
+                )
+                + "}"
             )
-        elif "optional" in type:
+        if "optional" in type:
             return "{" + self.instancestr(type.optional.type) + "}"
-        elif "oneof" in type:
+        if "oneof" in type:
             return self.inlineStructInstanceStr(
                 self.msgmap[random.choice(type.oneof.msg_names)]
             )
@@ -119,12 +141,11 @@ def file_trailer(namespace):
 def generate_code_and_tests(ast):
     """ Walk concord message format(CMF) AST and generate C++ code and C++ tests"""
     namespace = "cmf::test"
-    max_size = 5
     code, msgmap = cppgen.translate(ast, namespace)
     test_code = file_header(namespace)
     for msg in ast.msgs:
         # We generate `max_size` msg instances for tests
-        gens = [InstanceGen(i, msgmap) for i in range(0, max_size)]
+        gens = [InstanceGen(i, msgmap) for i in range(0, MAX_SIZE)]
         for g in gens:
             test_code += g.structInstanceVariableStr(msg) + "\n\n"
     return code, test_code + file_trailer(namespace)
